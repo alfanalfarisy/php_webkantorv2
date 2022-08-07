b63877433587b783efe2c0561c986292173cb6d7
alfanalfarisy <alfnalfrsss@gmail.com>
2022-08-07 16:28:10 +0000

    ok

--- components/home/peringatandini.php
+++ components/home/peringatandini.php
@@ -1,40 +1,44 @@
-    <!-- ======= PERINGATAN DINI ======= -->
+<!-- ======= PERINGATAN DINI ======= -->
 
-    <div class="modal fade" id="modalPeringatanDini" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
+<div class="modal fade" id="modalPeringatanDini" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
 
-                    <h4 class="modal-title">Peringatan Dini</h4>
-                </div>
-                <div id="warninglistmodal" class="modal-body" style="height: 600px;overflow:auto;"></div>
-
+                <h4 class="modal-title">Peringatan Dini</h4>
             </div>
+            <div id="warninglistmodal" class="modal-body" style="height: 600px;overflow:auto;"></div>
+
         </div>
     </div>
+</div>
 
 
 
-    <!-- ======= PERINGATAN DINI ======= -->
-    <section id="peringatandini-details" class="peringatandini-details peringatan-dini-home">
-        <div class="container">
-            <div class="row gy-4">
-                <div class="peringatan-dini-home-head col-md-2 no-padding">
+<!-- ======= PERINGATAN DINI ======= -->
+<section id="peringatandini-details" class="peringatandini-details peringatan-dini-home">
+    <div class="container">
+        <div class="row g-0">
+            <div class="col-md-2">
+
+                <div class="peringatan-dini-home-head ">
                     <h4><span></span>Peringatan Dini</h4>
                 </div>
-                <div id="alert" class="alert  col-md-10">
-
+            </div>
+            <div class="col-md-10">
+                <div id="alert" class="alert  ">
                     <div class="text-center">
-                        <p id="warningListTgl"></p>
-                        <p>
-                        <div id="warninglist"></div>
+                        <strong>
+                            <span id="warningListTgl"></span></strong>
+                        <p style="font-size: 13px;">
+                            <span id="warninglist"></span>
                         </p>
                     </div>
 
                 </div>
             </div>
         </div>
-    </section>
-    <!-- ======= END PERINGATAN DINI ======= -->
-    <!-- ======= END PERINGATAN DINI ======= -->
+    </div>
+</section>
+<!-- ======= END PERINGATAN DINI ======= -->
+<!-- ======= END PERINGATAN DINI ======= -->
